9fa52cc70c193185411bac4aac6f531f9dff53cc
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-07-14 10:09:21 -0300

    first commit

---
+++ src/test/kotlin/com/ewecarreira/car/CarApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ewecarreira.car
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
